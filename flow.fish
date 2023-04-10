e9d67312b651016f7811575e35b7225938c9068f
Conradin Laux <conradin.laux@gmail.com>
2023-04-10 18:33:35 +0200

    chore: update flow.fish

--- flow.fish
+++ flow.fish
@@ -2,8 +2,8 @@
 
 set -l d (date +%s)
 echo {$d}.asm
-cp crates/lexer/testdata/printf1.asm {$d}.asm
-silicon {$d}.asm --output before-{$d}.png
+cp ./crates/fmt/testdata/printf2.asm {$d}.asm
+silicon {$d}.asm --output before.png
 cargo run -- {$d}.asm > {$d}-out.asm
-silicon {$d}-out.asm --output after-{$d}.png
+silicon {$d}-out.asm --output after.png
 /bin/rm {$d}.asm {$d}-out.asm
